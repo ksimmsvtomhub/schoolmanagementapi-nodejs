fd161bf294975c4e55ed1c6f6bf2119be255dad4
Simms Kwarteng <kwartengsimms@gmail.com>
2023-11-07 20:42:26 +0000

    Initial Commit

---
+++ public/build/root-N23FRDYR.js
@@ -0,0 +1,104 @@
+import {
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-XU7DNSPJ.js";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts
+} from "/build/_shared/chunk-52W3EST6.js";
+import {
+  createHotContext
+} from "/build/_shared/chunk-XC35VUZZ.js";
+import "/build/_shared/chunk-UWV35TSL.js";
+import "/build/_shared/chunk-BOXFZXVX.js";
+import {
+  __toESM
+} from "/build/_shared/chunk-PNG5AS42.js";
+
+// app/root.tsx
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime(), 1);
+if (!window.$RefreshReg$ || !window.$RefreshSig$ || !window.$RefreshRuntime$) {
+  console.warn("remix:hmr: React Fast Refresh only works when the Remix compiler is running in development mode.");
+} else {
+  prevRefreshReg = window.$RefreshReg$;
+  prevRefreshSig = window.$RefreshSig$;
+  window.$RefreshReg$ = (type, id) => {
+    window.$RefreshRuntime$.register(type, '"app\\\\root.tsx"' + id);
+  };
+  window.$RefreshSig$ = window.$RefreshRuntime$.createSignatureFunctionForTransform;
+}
+var prevRefreshReg;
+var prevRefreshSig;
+if (import.meta) {
+  import.meta.hot = createHotContext(
+    //@ts-expect-error
+    "app\\root.tsx"
+  );
+  import.meta.hot.lastModified = "1699278419922.8213";
+}
+function App() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("html", { children: [
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("head", { children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("link", { rel: "icon", href: "data:image/x-icon;base64,AA" }, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 26,
+        columnNumber: 11
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Meta, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 27,
+        columnNumber: 11
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Links, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 28,
+        columnNumber: 11
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/root.tsx",
+      lineNumber: 25,
+      columnNumber: 9
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("body", { children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h1", { children: "Hello world!" }, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 31,
+        columnNumber: 11
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Outlet, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 32,
+        columnNumber: 11
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Scripts, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 33,
+        columnNumber: 11
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(LiveReload, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 34,
+        columnNumber: 11
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/root.tsx",
+      lineNumber: 30,
+      columnNumber: 9
+    }, this)
+  ] }, void 0, true, {
+    fileName: "app/root.tsx",
+    lineNumber: 24,
+    columnNumber: 10
+  }, this);
+}
+_c = App;
+var _c;
+$RefreshReg$(_c, "App");
+window.$RefreshReg$ = prevRefreshReg;
+window.$RefreshSig$ = prevRefreshSig;
+export {
+  App as default
+};
+//# sourceMappingURL=/build/root-N23FRDYR.js.map
